e299683272a456e95b9e51546231d6800d8f85c9
Abraham Sánchez <abraham@twintags.com>
2016-06-02 10:38:54 +0200

    Track changed attributes when a version is added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160524102641) do
+ActiveRecord::Schema.define(version: 20160527073357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,12 +73,13 @@ ActiveRecord::Schema.define(version: 20160524102641) do
   add_index "envelopes", ["processed_resource"], name: "index_envelopes_on_processed_resource", using: :gin
 
   create_table "versions", force: :cascade do |t|
-    t.string   "item_type",               null: false
-    t.integer  "item_id",                 null: false
-    t.string   "event",                   null: false
+    t.string   "item_type",                   null: false
+    t.integer  "item_id",                     null: false
+    t.string   "event",                       null: false
     t.string   "whodunnit"
-    t.jsonb    "object",     default: {}
+    t.jsonb    "object",         default: {}
     t.datetime "created_at"
+    t.text     "object_changes"
   end
 
   add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
