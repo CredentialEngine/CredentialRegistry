0929375b135cfffe6504a0a0f06b9e20386ec2d6
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2016-08-21 13:29:41 -0300

    Add searchbale concern to models for live indexing

--- app/models/extensions/searchable.rb
+++ app/models/extensions/searchable.rb
@@ -1,43 +1,37 @@
 require 'active_support/concern'
 require 'search/repository'
+require 'search/document'
 
 # When included define the search properties for the model
 module Searchable
   extend ActiveSupport::Concern
 
   included do
-    # attr_accessor :skip_indexing
-    #
-    # after_commit :index_document, on: [:create, :update], if: :should_index?
-    # after_commit :delete_document, on: :destroy, if: :should_index?
-    #
-    # def index_document
-    #   begin
-    #     doc = Search::Document.build_from self
-    #     search_repo.save(doc)
-    #   rescue Faraday::ConnectionFailed; end
-    # end
-    #
-    # def delete_document
-    #   begin
-    #     doc = Search::Document.build_from self
-    #     search_repo.delete(doc)
-    #   rescue Faraday::ConnectionFailed; end
-    # end
-    #
-    # def search_repo
-    #   @@search_repo ||= Search::Repository.new
-    # end
-    #
-    # def self.search(term, options={})
-    #   Search::Document.search(
-    #     term,
-    #     options.merge!(model_type: self.name.underscore)
-    #   )
-    # end
-    #
-    # def should_index?
-    #   !skip_indexing && search_repo.index_exists?
-    # end
+    attr_accessor :skip_indexing
+
+    after_commit :index_document, on: [:create, :update], if: :should_index?
+    after_commit :delete_document, on: :destroy, if: :should_index?
+
+    def index_document
+      Search::Document.build(self).index!
+      # rescue Faraday::ConnectionFailed; end
+    end
+
+    def delete_document
+      Search::Document.build(self).delete!
+      # rescue Faraday::ConnectionFailed; end
+    end
+
+    def self.search_repo
+      @search_repo ||= Search::Repository.new
+    end
+
+    def search_index_exists?
+      self.class.search_repo.index_exists?
+    end
+
+    def should_index?
+      !skip_indexing && search_index_exists?
+    end
   end
 end
