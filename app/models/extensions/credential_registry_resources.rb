a5e4fb0f233a1449c0aae9a4fc3c66ee2b7b8aa1
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2016-08-30 17:52:31 -0300

    Validate unique ctid

--- app/models/extensions/credential_registry_resources.rb
+++ app/models/extensions/credential_registry_resources.rb
@@ -5,6 +5,25 @@ module CredentialRegistryResources
   extend ActiveSupport::Concern
 
   included do
+    scope :with_ctid, (lambda do |ctid|
+      where('processed_resource @> ?', { 'ctdl:ctid' => ctid }.to_json)
+    end)
+
+    validate :unique_ctid, if: :credential_registry?
+
+    def unique_ctid
+      if Envelope.in_community('credential_registry')
+                 .where.not(envelope_id: envelope_id)
+                 .with_ctid(processed_resource['ctdl:ctid'])
+                 .exists?
+        errors.add :resource, 'CTID must be unique'
+      end
+    end
+
+    def credential_registry?
+      community_name == 'credential_registry'
+    end
+
     def self.generate_ctid
       "urn:guid:#{SecureRandom.uuid}"
     end
