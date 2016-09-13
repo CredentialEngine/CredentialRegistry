843436e8cbcc8d22ff3e8b60343deea95074893b
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2016-09-13 14:00:01 -0300

    Fix cloud66 again =/

--- .cloud66/install_swagger.sh
+++ .cloud66/install_swagger.sh
@@ -1,5 +1,5 @@
 wget https://github.com/swagger-api/swagger-ui/archive/v2.2.3.zip
-tar zx v2.2.3.zip
+tar xzvf v2.2.3.zip
 mv swagger-ui-2.2.3/dist $STACK_PATH/public/swagger
 rm -rf swagger-ui-2.2.3/
 sed -i -e 's,http://petstore.swagger.io/v2,,g' $STACK_PATH/public/swagger/index.html
