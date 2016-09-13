601ed667328161503e592cf7eb912c0cdfb4b558
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2016-09-13 13:51:34 -0300

    Try to fix again cloud66 deploy

--- .cloud66/install_swagger.sh
+++ .cloud66/install_swagger.sh
@@ -1,4 +1,5 @@
-cd $STACK_PATH/tmp
-curl -L https://github.com/swagger-api/swagger-ui/archive/v2.2.3.zip | tar zx
-mv $STACK_PATH/tmp/swagger-ui-2.2.3/dist $STACK_PATH/public/swagger
-sed -i '' -e 's,http://petstore.swagger.io/v2,,g' $STACK_PATH/public/swagger/index.html
+wget https://github.com/swagger-api/swagger-ui/archive/v2.2.3.zip
+tar zx v2.2.3.zip
+mv swagger-ui-2.2.3/dist $STACK_PATH/public/swagger
+rm -rf swagger-ui-2.2.3/
+sed -i -e 's,http://petstore.swagger.io/v2,,g' $STACK_PATH/public/swagger/index.html
