1ce29f2adab4f2445dbea9e010e01a8570efc770
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2016-11-08 13:28:47 -0200

    Allow schema updates + validate using stored schemas

--- app/models/extensions/json_schema_resources.rb
+++ app/models/extensions/json_schema_resources.rb
@@ -9,16 +9,16 @@ module JsonSchemaResources
       unscoped.where(envelope_type: envelope_types['json_schema'])
     end)
 
-    scope :with_schema_name, (lambda do |name|
-      where('processed_resource @> ?', { 'name' => name }.to_json)
+    scope :with_name, (lambda do |name|
+      schemas.where('processed_resource @> ?', { 'name' => name }.to_json)
     end)
 
     validate :unique_json_schema_name, if: :json_schema?
 
     def unique_json_schema_name
       if Envelope.schemas
+                 .with_name(processed_resource['name'])
                  .where.not(envelope_id: envelope_id)
-                 .with_schema_name(processed_resource['name'])
                  .exists?
         errors.add :resource, 'Schema name must be unique'
       end
