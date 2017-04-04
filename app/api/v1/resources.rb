172b85f1e05460743f79a337c006f2cc3df90d29
Maximilian Haack <max@holmes-iv.com>
2017-04-04 15:49:09 +0800

    Add CREATE resource

--- app/api/v1/resources.rb
+++ app/api/v1/resources.rb
@@ -6,7 +6,47 @@ module API
 
       helpers SharedHelpers
 
+      before_validation { normalize_envelope_community }
+
       resource :resources do
+        desc 'Publishes a new envelope',
+             http_codes: [
+               { code: 201, message: 'Envelope created' },
+               { code: 200, message: 'Envelope updated' }
+             ]
+        helpers do
+          def update_if_exists?
+            @update_if_exists ||= params.delete(:update_if_exists)
+          end
+
+          def skip?
+            @skip_validation ||= params.delete(:skip_validation)
+          end
+        end
+        params do
+          optional :update_if_exists,
+                   type: Grape::API::Boolean,
+                   desc: 'Whether to update the envelope if it already exists',
+                   documentation: { param_type: 'query' }
+          optional :skip_validation,
+                   type: Grape::API::Boolean,
+                   desc: 'Whether to skip validations if the community allows',
+                   documentation: { param_type: 'query' }
+        end
+        post do
+          envelope, errors = EnvelopeBuilder.new(
+            params, update_if_exists: update_if_exists?, skip_validation: skip?
+          ).build
+
+          if errors
+            json_error! errors, [:envelope, envelope.try(:resource_schema_name)]
+
+          else
+            present envelope, with: API::Entities::Envelope
+            update_if_exists? ? status(:ok) : status(:created)
+          end
+        end
+
         desc 'Return a resource.'
         params do
           requires :id, type: String, desc: 'Resource id.'
--- spec/api/v1/resources_spec.rb
+++ spec/api/v1/resources_spec.rb
@@ -1,4 +1,23 @@
 describe API::V1::Resources do
+  let!(:ec) { create(:envelope_community, name: 'ce_registry') }
+
+  context 'CREATE /api/resources' do
+    before do
+      post '/api/resources', attributes_for(:envelope, :from_cer,
+                                            envelope_community: ec.name)
+    end
+
+    it 'returns a 201 Created http status code' do
+      expect_status(:created)
+    end
+
+    context 'returns the newly created envelope' do
+      it { expect_json_types(envelope_id: :string) }
+      it { expect_json(envelope_community: 'ce_registry') }
+      it { expect_json(envelope_version: '0.52.0') }
+    end
+  end
+
   context 'GET /api/resources/:id' do
     let!(:envelope)  { create(:envelope, :from_cer, :with_cer_credential) }
     let!(:resource)  { envelope.processed_resource }
