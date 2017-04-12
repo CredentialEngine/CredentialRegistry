015c6f2098e258aa95966e341c62bc4a21a81225
Maximilian Haack <max@holmes-iv.com>
2017-04-13 00:05:53 +0800

    Update swagger UI on cloud66

--- .cloud66/install_swagger.sh
+++ .cloud66/install_swagger.sh
@@ -1,6 +1,6 @@
-wget https://github.com/swagger-api/swagger-ui/archive/v2.2.3.zip
-unzip  v2.2.3.zip
-mv swagger-ui-2.2.3/dist $STACK_PATH/public/swagger
-rm -rf swagger-ui-2.2.3/
-rm v2.2.3.zip
+wget https://github.com/swagger-api/swagger-ui/archive/v3.0.5.zip
+unzip  v3.0.5.zip
+mv swagger-ui-3.0.5/dist $STACK_PATH/public/swagger
+rm -rf swagger-ui-3.0.5/
+rm v3.0.5.zip
 sed -i -e 's,http://petstore.swagger.io/v2,,g' $STACK_PATH/public/swagger/index.html
