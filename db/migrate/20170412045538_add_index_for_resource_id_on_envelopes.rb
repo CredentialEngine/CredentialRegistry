6645c3bcbf8e859796f2422f974dfdc92c443bf4
Maximilian Haack <max@holmes-iv.com>
2017-04-12 13:29:36 +0800

    Add PG Index for Resource IDs in Envelopes

---
+++ db/migrate/20170412045538_add_index_for_resource_id_on_envelopes.rb
@@ -0,0 +1,10 @@
+class AddIndexForResourceIdOnEnvelopes < ActiveRecord::Migration
+  def up
+    execute('CREATE INDEX envelopes_resources_id_idx ON envelopes ' \
+            '((processed_resource->>\'@id\'));')
+  end
+
+  def down
+    execute("DROP INDEX envelopes_resources_id_idx")
+  end
+end
