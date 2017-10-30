93172d60d4aae923d9049d35c848da756f0cf526
Abraham Sánchez <abraham@twintags.com>
2017-10-30 10:12:31 +0100

    Add Cloud 66 hook to install the postgres client

---
+++ .cloud66/install_postgresql_client.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+add-apt-repository "deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main"
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+apt-get update
+apt-get install postgresql-9.6 -y
