458ab48036709b617b2bca39b115a7291dd7d72e
Ranko Radonić <ranko.radonic@gmx.com>
2017-11-14 18:26:46 -0500

    Seed database with an admin, publisher, and user

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,7 @@ EnvelopeCommunity.create([{ name: 'learning_registry',
                           { name: 'ce_registry',
                             backup_item: 'ce-registry-test',
                             default: true }])
+
+admin = Admin.create(name: 'ce_admin')
+publisher = Publisher.create(name: 'ce_admin', admin: admin)
+User.create(email: 'user@ce.org', publisher: publisher)
