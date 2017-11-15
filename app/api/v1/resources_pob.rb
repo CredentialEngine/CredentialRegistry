33cac069bc67e54b2ee9770ac79a2f6a76d41947
Ranko Radonić <ranko.radonic@gmx.com>
2017-11-15 10:28:28 -0500

    Add publish on demand endpoint
    It's only a stub right now.

---
+++ app/api/v1/resources_pob.rb
@@ -0,0 +1,31 @@
+module API
+  module V1
+    # Default options for all API endpoints and versions
+    module ResourcesPob
+      extend ActiveSupport::Concern
+
+      included do
+        desc 'Takes a resource and an organization id, signs the resource '\
+             'on behalf of an organization, and publishes a new envelope with '\
+             'that signed resource',
+             http_codes: [
+               { code: 201, message: 'Envelope created' },
+               { code: 200, message: 'Envelope updated' }
+             ]
+        before do
+          authenticate!
+        end
+        params do
+          use :update_if_exists
+          use :skip_validation
+        end
+        post 'organizations/:organization_id/documents' do
+          params[:envelope_community] = select_community
+
+          puts params[:envelope_community]
+          puts params[:organization_id]
+        end
+      end
+    end
+  end
+end
