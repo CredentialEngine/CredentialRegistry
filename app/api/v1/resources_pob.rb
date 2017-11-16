a74f80bc2a8f1536077ecd1087bff185cfb7612f
Ranko Radonić <ranko.radonic@gmx.com>
2017-11-16 12:10:37 -0500

    Implement publish on behalf endpoint
    We only allow publishing on behalf of an organization the user is
    authorized with, so no super publisher functionality yet.

--- app/api/v1/resources_pob.rb
+++ app/api/v1/resources_pob.rb
@@ -12,16 +12,55 @@ module API
                { code: 201, message: 'Envelope created' },
                { code: 200, message: 'Envelope updated' }
              ]
+
         params do
           use :update_if_exists
           use :skip_validation
         end
+
+        # rubocop:disable Metrics/BlockLength
         post 'organizations/:organization_id/documents' do
           authenticate!
           params[:envelope_community] = select_community
 
-          puts params[:envelope_community]
-          puts params[:organization_id]
+          organization = Organization.find(params[:organization_id])
+          publisher = current_user.publisher
+          organization_publisher = OrganizationPublisher
+                                   .where(organization: organization)
+                                   .where(publisher: publisher)
+                                   .first
+
+          unless organization_publisher
+            error!(
+              'User\'s publisher is not authorized to publish on behalf of this organization',
+              422
+            )
+          end
+
+          key_pair = organization_publisher.key_pairs.first
+
+          envelope_attributes = {
+            'envelope_type': 'resource_data',
+            'envelope_version': '1.0.0',
+            'envelope_community': 'ce_registry',
+            'resource': JWT.encode(
+              env['api.request.body'],
+              OpenSSL::PKey::RSA.new(key_pair.private_key),
+              'RS256'
+            ),
+            'resource_format': 'json',
+            'resource_encoding': 'jwt',
+            'resource_public_key': key_pair.public_key
+          }
+
+          envelope, errors = EnvelopeBuilder.new(envelope_attributes).build
+
+          if errors
+            json_error! errors, [:envelope, envelope.try(:resource_schema_name)]
+          else
+            present envelope, with: API::Entities::Envelope
+            update_if_exists? ? status(:ok) : status(:created)
+          end
         end
       end
     end
