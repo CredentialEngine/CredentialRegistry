eb2e0e4fdf321f1693f3a9b0ca587a1cfb343dc8
Abraham Sánchez <abraham@twintags.com>
2017-11-29 12:58:26 +0100

    Build graph search for assessments & learning opp
    It also tweaks the search logic to use UNION queries to take into
    account all the possible paths.

--- app/services/graph_search.rb
+++ app/services/graph_search.rb
@@ -10,23 +10,51 @@ class GraphSearch
 
   def initialize
     @query_service = Neo4j::Session.query
+    @params = { limit_100: 100 }
   end
 
   def organizations(conditions = [], roles = [])
-    query = query_service.match("(organization)-[#{convert_roles(roles)}]-(credential)")
-                         .where(credential_clause)
-                         .where(organization_clause)
-    perform(query, 'organization', conditions)
+    active_roles = convert_roles(roles)
+    cypher_queries = %w[credential assessment_profile learning_opportunity_profile].map do |target|
+      next if inapplicable_conditions?(conditions, [target, 'organization'])
+
+      path = "(organization)-[#{active_roles}]-(#{target})"
+      @query = query_service.match(path).where(send("#{target}_clause")).where(organization_clause)
+      perform_filtering('organization', conditions).to_cypher
+    end
+    perform_union(cypher_queries, 'organization')
   end
 
   def credentials(conditions = [], roles = [])
-    query = query_service.match("(credential)-[#{convert_roles(roles)}]-(organization)")
-                         .where(credential_clause)
-                         .where(organization_clause)
-    perform(query, 'credential', conditions)
+    @query = query_service.match("(credential)-[#{convert_roles(roles)}]-(organization)")
+                          .where(credential_clause)
+                          .where(organization_clause)
+    perform_filtering('credential', conditions).pluck('distinct credential')
+  end
+
+  %w[assessment_profiles learning_opportunity_profiles].each do |method|
+    define_method(method) do |conditions = [], roles = []|
+      entity = Dry::Inflector.new.singularize(method)
+      @query = query_service.match("(#{entity})-[#{convert_roles(roles)}]-(organization)")
+                            .where(send("#{entity}_clause"))
+                            .where(organization_clause)
+      perform_filtering(entity, conditions).pluck("distinct #{entity}")
+    end
   end
 
   private
+
+  #
+  # Determines whether some condition in the current query is not applicable (because it references
+  # an entity not present in the current path). When that happens the query can not progress
+  # further and therefore not executed.
+  #
+  def inapplicable_conditions?(conditions, accepted_variables)
+    conditions.reject do |condition|
+      variable = extract_variable(condition.object.value || accepted_variables.last)
+      accepted_variables.include?(variable)
+    end.any?
+  end
 
   def organization_clause(variable = 'organization')
     "#{variable}:CredentialOrganization OR #{variable}:QACredentialOrganization"
@@ -36,6 +64,14 @@ class GraphSearch
     credential_types.map { |type| "#{variable}:#{type}" }.join(' OR ')
   end
 
+  def assessment_profile_clause(variable = 'assessment_profile')
+    "#{variable}:AssessmentProfile"
+  end
+
+  def learning_opportunity_profile_clause(variable = 'learning_opportunity_profile')
+    "#{variable}:LearningOpportunityProfile"
+  end
+
   def credential_types
     %w[ApprenticeshipCertificate AssociateDegree BachelorDegree Badge Certificate Certification
        Degree DigitalBadge Diploma DoctoralDegree GeneralEducationDevelopment JourneymanCertificate
@@ -43,10 +79,10 @@ class GraphSearch
        QualityAssuranceCredential ResearchDoctorate SecondarySchoolDiploma]
   end
 
-  def perform(query, main_variable, conditions = [])
-    @query = query
+  def perform_filtering(main_variable, conditions = [])
     parse_conditions(conditions).each { |condition| apply_condition(condition, main_variable) }
-    @query.return(main_variable).limit(100).pluck("distinct #{main_variable}")
+    @params.merge!(@query.parameters)
+    @query.return("distinct (#{main_variable})").limit(100)
   end
 
   def parse_conditions(conditions)
@@ -60,8 +96,8 @@ class GraphSearch
   def apply_condition(condition, main_variable)
     element = File.basename(condition.element)
     variable = extract_variable(condition.object.value || main_variable)
-    relation_variable = build_composite_match(condition, variable)
-    @query = @query.where(where_clause(element, condition.value, relation_variable))
+    composite_variable = build_composite_match(condition, variable)
+    @query = where_clause(@query, element, condition.value, composite_variable)
   end
 
   #
@@ -73,12 +109,16 @@ class GraphSearch
   def build_composite_match(condition, variable)
     relations = File.dirname(condition.element).split('/').reject { |r| r == '.' }
     if relations.any?
-      relation_variable = random_variable
-      relations_clause = relations.map { |relation| "[:#{relation}]" }.join('-()-')
-      @query = @query.match("(#{variable})-#{relations_clause}-(#{relation_variable})")
-      relation_variable
+      composite_variable = random_variable
+      composite_clause = relations.map { |relation| "[:#{relation}]" }.join('-()-')
+      @query = @query.match("(#{variable})-#{composite_clause}-(#{composite_variable})")
+      composite_variable
     else
       variable
     end
+  end
+
+  def perform_union(queries, entity)
+    Neo4j::Session.query(queries.compact.join(' UNION '), @params).map { |c| c[entity] }
   end
 end
--- spec/services/graph_search_spec.rb
+++ spec/services/graph_search_spec.rb
@@ -2,16 +2,18 @@ require_relative '../../app/services/graph_search'
 require_relative '../../app/models/query_condition'
 
 describe GraphSearch, type: :service do
+  let(:graph_search) { GraphSearch.new }
+
   before(:all) do
     reset_neo4j
-    import_into_neo4j('../../support/fixtures/json/ce_registry/credential/3_import.json')
-    import_into_neo4j('../../support/fixtures/json/ce_registry/credential/4_import.json')
+    %w[credential/3 credential/4 assessment_profile/2 assessment_profile/3
+       learning_opportunity_profile/2 learning_opportunity_profile/3].each do |file|
+      import_into_neo4j("../../support/fixtures/json/ce_registry/#{file}_import.json")
+    end
   end
 
   describe '#organizations' do
-    let(:graph_search) { GraphSearch.new }
-
-    it 'returns organizations according to the some conditions' do
+    it 'returns organizations according to the conditions related to credentials' do
       conditions = [QueryCondition.new(object: 'Credential',
                                        element: 'renewal/name',
                                        value: 'Health Informatics'),
@@ -25,9 +27,35 @@ describe GraphSearch, type: :service do
       expect(organizations.last.props[:name]).to eq('Indiana University Bloomington')
     end
 
+    it 'returns organizations according to the conditions related to assessments' do
+      conditions = [QueryCondition.new(object: 'AssessmentProfile',
+                                       element: 'estimatedCost/audienceType/targetNodeName',
+                                       value: 'Citizen'),
+                    QueryCondition.new(element: 'agentSectorType/targetNodeName', value: 'Public')]
+
+      organizations = graph_search.organizations(conditions)
+
+      expect(organizations.size).to eq(1)
+      expect(organizations.last.props[:type]).to eq('QACredentialOrganization')
+      expect(organizations.last.props[:name]).to eq('Indiana State Board of Nursing')
+    end
+
+    it 'returns organizations according to the conditions related to learning opportunities' do
+      conditions = [QueryCondition.new(object: 'LearningOpportunityProfile',
+                                       element: 'estimatedCost/price',
+                                       value: 1640),
+                    QueryCondition.new(element: 'foundingDate', value: '1971')]
+
+      organizations = graph_search.organizations(conditions)
+
+      expect(organizations.size).to eq(1)
+      expect(organizations.last.props[:type]).to eq('QACredentialOrganization')
+      expect(organizations.last.props[:name]).to eq('Indiana Commission for Higher Education')
+    end
+
     it 'returns nothing when conditions do not match any record' do
-      conditions = [QueryCondition.new(object: 'Credential',
-                                       element: 'renewal/name',
+      conditions = [QueryCondition.new(object: 'AssessmentProfile',
+                                       element: 'estimatedCost/price',
                                        value: 'WRONG VALUE')]
 
       organizations = graph_search.organizations(conditions)
@@ -46,8 +74,6 @@ describe GraphSearch, type: :service do
   end
 
   describe '#credentials' do
-    let(:graph_search) { GraphSearch.new }
-
     it 'returns credentials according to the some conditions' do
       conditions = [QueryCondition.new(object: 'Organization',
                                        element: 'address/addressLocality',
@@ -71,13 +97,82 @@ describe GraphSearch, type: :service do
       expect(credentials).to be_empty
     end
 
-    it 'filters organizations according to the roles' do
+    it 'filters credentials according to the roles' do
       roles = %w[OFFERED REVOKED]
 
       credentials = graph_search.credentials([], roles)
 
       expect(credentials.size).to eq(1)
       expect(credentials.last.props[:name]).to eq('Health Informatics')
+    end
+  end
+
+  describe '#assessments' do
+    it 'returns assessments according to the some conditions' do
+      conditions = [QueryCondition.new(object: 'Organization',
+                                       element: 'targetContactPoint/name',
+                                       value: 'Organization Contact Information'),
+                    QueryCondition.new(element: 'hasGroupEvaluation', value: false)]
+
+      assessments = graph_search.assessment_profiles(conditions, ['OWNED'])
+
+      expect(assessments.size).to eq(1)
+      expect(assessments.last.props[:type]).to eq('AssessmentProfile')
+      expect(assessments.last.props[:name]).to eq('Pharmacy Technician Program')
+    end
+
+    it 'returns nothing when conditions do not match any record' do
+      conditions = [QueryCondition.new(object: 'Organization',
+                                       element: 'description',
+                                       value: 'WRONG VALUE')]
+
+      assessments = graph_search.assessment_profiles(conditions)
+
+      expect(assessments).to be_empty
+    end
+
+    it 'filters assessments according to the roles' do
+      roles = %w[REGULATED]
+
+      assessments = graph_search.assessment_profiles([], roles)
+
+      expect(assessments.size).to eq(1)
+      expect(assessments.last.props[:name]).to eq('Certified Registered Nurse Anesthetist (CRNA)')
+    end
+  end
+
+  describe '#learning_opportunities' do
+    it 'returns learning opportunities according to the some conditions' do
+      conditions = [QueryCondition.new(object: 'Organization',
+                                       element: 'subjectWebpage',
+                                       value: 'http://www.iue.edu'),
+                    QueryCondition.new(element: 'jurisdiction/globalJurisdiction', value: true)]
+
+      assessments = graph_search.learning_opportunity_profiles(conditions, ['REGULATED'])
+
+      expect(assessments.size).to eq(1)
+      expect(assessments.last.props[:type]).to eq('LearningOpportunityProfile')
+      expect(assessments.last.props[:name]).to eq('Certified Welder Courses')
+    end
+
+    it 'returns nothing when conditions do not match any record' do
+      conditions = [QueryCondition.new(object: 'Organization',
+                                       element: 'subjectWebpage',
+                                       value: 'WRONG VALUE')]
+
+      assessments = graph_search.learning_opportunity_profiles(conditions)
+
+      expect(assessments).to be_empty
+    end
+
+    it 'filters learning opportunities according to the roles' do
+      roles = %w[ACCREDITED]
+      name = 'Certified Radiographic Interpreter (CRI) Seminars'
+
+      assessments = graph_search.learning_opportunity_profiles([], roles)
+
+      expect(assessments.size).to eq(1)
+      expect(assessments.last.props[:name]).to eq(name)
     end
   end
 end
