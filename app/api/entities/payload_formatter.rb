509e2bf0d605255c71d09621398bf9147091abfd
Rômulo Saksida <romulo@rsaksida.com>
2018-10-16 22:53:11 -0300

    Fix bug in JSON k/v ordering
    Don't assign null properties

--- app/api/entities/payload_formatter.rb
+++ app/api/entities/payload_formatter.rb
@@ -1,13 +1,14 @@
 # Adds a method for formatting payload contents.
 module PayloadFormatter
-  extend self
-
   def format_payload(payload)
     new_payload = {}
-    new_payload['@context'] = payload.delete('@context')
-    new_payload['@id'] = payload.delete('@id')
-    new_payload['@graph'] = payload.delete('@graph')
+    ['@context', '@id', '@graph'].each do |key|
+      val = payload.delete(key)
+      new_payload[key] = val if val.present?
+    end
     payload.each { |k, v| new_payload[k] = v }
     new_payload
   end
+
+  module_function :format_payload
 end
