8dfc0a7d22b7fe4aab17b0397093b9fc25c2c788
Alex Nizamov <nizamov.alex@gmail.com>
2019-09-18 18:01:41 +0500

    Support @graph in publish on behalf API

--- spec/api/v1/publish_spec.rb
+++ spec/api/v1/publish_spec.rb
@@ -3,9 +3,11 @@ RSpec.describe API::V1::Publish do
     let!(:ec) { create(:envelope_community, name: 'ce_registry') }
     let(:user) { create(:user) }
     let(:user2) { create(:user) }
+
     let(:resource_json) do
-      content = File.read MR.root_path.join('db', 'seeds', 'ce_registry', 'credential.json')
-      JSON.parse(content).first.to_json
+      File.read(
+        MR.root_path.join('db', 'seeds', 'ce_registry', 'credential.json')
+      )
     end
 
     context 'publish on behalf without token' do
@@ -26,19 +28,19 @@ RSpec.describe API::V1::Publish do
         organization = create(:organization)
         create(:organization_publisher, organization: organization, publisher: user.publisher)
 
-        post "/resources/organizations/#{organization.id}/documents",
+        post "/resources/organizations/#{organization.id}/documents?skip_validation=true",
              resource_json, 'Authorization' => 'Token ' + user.auth_token.value
       end
 
-      it 'returns a 201 created http status code' do
+      it 'returns the newly created envelope with a 201 Created HTTP status code' do
         expect_status(:created)
-      end
-
-      context 'returns the newly created envelope' do
-        it { expect_json_types(envelope_id: :string) }
-        it { expect_json(envelope_community: 'ce_registry') }
-        it { expect_json(envelope_version: '1.0.0') }
-        it { expect_json(changed: true) }
+        expect_json_types(envelope_id: :string)
+        expect_json(envelope_ceterms_ctid: 'ce-53bc7e5d-d39c-4687-ac89-0474f691055d')
+        expect_json(envelope_ctdl_type: 'ceterms:MasterDegree')
+        expect_json(envelope_community: 'ce_registry')
+        expect_json(envelope_version: '1.0.0')
+        expect_json(secondary_publisher_id: nil)
+        expect_json(changed: true)
       end
     end
 
@@ -47,24 +49,21 @@ RSpec.describe API::V1::Publish do
         organization = create(:organization)
         create(:organization_publisher, organization: organization, publisher: user.publisher)
 
-        post "/resources/organizations/#{organization.id}/documents",
+        post "/resources/organizations/#{organization.id}/documents?skip_validation=true",
              resource_json,
              'Authorization' => 'Token ' + user.auth_token.value,
              'Secondary-Token' => 'Token ' + user2.auth_token.value
       end
 
-      it 'returns a 201 created http status code' do
+      it 'returns the newly created envelope with a 201 Created HTTP status code' do
         expect_status(:created)
-      end
-
-      context 'returns the newly created envelope' do
-        it { expect_json_types(envelope_id: :string) }
-        it { expect_json_types(envelope_ceterms_ctid: :string) }
-        it { expect_json_types(envelope_ctdl_type: :string) }
-        it { expect_json(envelope_community: 'ce_registry') }
-        it { expect_json(envelope_version: '1.0.0') }
-        it { expect_json(secondary_publisher_id: user2.publisher.id) }
-        it { expect_json(changed: true) }
+        expect_json_types(envelope_id: :string)
+        expect_json(envelope_ceterms_ctid: 'ce-53bc7e5d-d39c-4687-ac89-0474f691055d')
+        expect_json(envelope_ctdl_type: 'ceterms:MasterDegree')
+        expect_json(envelope_community: 'ce_registry')
+        expect_json(envelope_version: '1.0.0')
+        expect_json(secondary_publisher_id: user2.publisher.id)
+        expect_json(changed: true)
       end
     end
 
@@ -89,19 +88,19 @@ RSpec.describe API::V1::Publish do
         organization = create(:organization)
 
         token = "Token #{super_publisher_user.auth_tokens.first.value}"
-        post "/resources/organizations/#{organization.id}/documents",
+        post "/resources/organizations/#{organization.id}/documents?skip_validation=true",
              resource_json, 'Authorization' => token
       end
 
-      it 'returns a 201 created http status code' do
+      it 'returns the newly created envelope with a 201 Created HTTP status code' do
         expect_status(:created)
-      end
-
-      context 'returns the newly created envelope' do
-        it { expect_json_types(envelope_id: :string) }
-        it { expect_json(envelope_community: 'ce_registry') }
-        it { expect_json(envelope_version: '1.0.0') }
-        it { expect_json(changed: true) }
+        expect_json_types(envelope_id: :string)
+        expect_json(envelope_ceterms_ctid: 'ce-53bc7e5d-d39c-4687-ac89-0474f691055d')
+        expect_json(envelope_ctdl_type: 'ceterms:MasterDegree')
+        expect_json(envelope_community: 'ce_registry')
+        expect_json(envelope_version: '1.0.0')
+        expect_json(secondary_publisher_id: nil)
+        expect_json(changed: true)
       end
     end
 
