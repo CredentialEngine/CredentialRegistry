2337910a996bf67e13a18c9e99ac55f79880e55a
Alex Nizamov <nizamov.alex@gmail.com>
2020-08-13 17:08:35 +0500

    Move pre-calculated description sets from SPARQL to Postgres

--- app/services/precalculate_description_sets.rb
+++ app/services/precalculate_description_sets.rb
@@ -1,63 +1,78 @@
-# Inserts pre-calculated description set statements into the SPARQL DB
+# Creates or updates description sets built from SPARQL data
 class PrecalculateDescriptionSets
   class << self
     def process(envelope)
       envelope.processed_resource.fetch('@graph').each do |resource|
-        type = resource.fetch('@type')
-        items = maps[type]
-        next unless items
+        ctid = resource['ceterms:ctid']
+        next unless ctid.present?
 
-        insert_description_set(resource.fetch('@id'), items)
+        description_sets = maps
+          .select { |m| m[:types].include?(resource.fetch('@type')) }
+          .map do |map|
+            build_description_sets(map, ctid)
+          end.flatten
+
+        insert_description_sets(description_sets)
       end
     end
 
     def process_all
-      maps.each do |type, items|
-        EnvelopeResource.where(resource_type: type).find_each do |resource|
-          uri = resource.processed_resource.fetch('@id')
-          insert_description_set(uri, items)
-        end
+      maps.each do |map|
+        insert_description_sets(build_description_sets(map))
       end
     end
 
     private
 
-    def insert_description_set(uri, items)
-      conditions = []
-      nodes = []
+    def build_description_sets(map, ctid = nil)
+      path = map.fetch(:path)
+      query = map.fetch(:query)
+      types = map.fetch(:types)
 
-      items.each_with_index do |item, index|
-        target = "?target#{index + 1}"
-        conditions << "{ #{item[:query].gsub('?target', target)} }"
-
-        nodes << <<~NODE
-          credreg:__relatedItemsMap [
-            credreg:__dspPath '#{item[:path]}';
-            credreg:__dspURI #{target}
-          ]
-        NODE
-      end
-
-      statement = <<~SPARQL
+      query = <<~SPARQL
         PREFIX ceasn: <https://purl.org/ctdlasn/terms/>
         PREFIX ceterms: <https://purl.org/ctdl/terms/>
         PREFIX credreg: <https://credreg.net/>
         PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
 
-        INSERT
-        {
-          ?subject credreg:__descriptionSet [
-            #{nodes.join(";\n")}
-          ]
-        }
+        SELECT ?ctid (GROUP_CONCAT(?target) AS ?uris)
         WHERE
         {
-          VALUES ?subject { <#{uri}> }
-          #{conditions.join("\nUNION\n")}
+          #{"BIND('#{ctid}' AS ?ctid)" if ctid}
+          VALUES ?type { #{types.join(' ')} }
+          ?subject a ?type .
+          ?subject ceterms:ctid ?ctid .
+          #{query}
         }
+        GROUP BY ?ctid
       SPARQL
 
-      QuerySparql.call('update' => statement)
+      response = QuerySparql.call('query' => query)
+      
+      if response.status != 200
+        MR.logger.error(
+          "PrecalculateDescriptionSets -- Failed to execute query: #{query}"
+        )
+
+        return []
+      end
+
+      JSON(response.result).dig('results', 'bindings').map do |binding|
+        description_set = DescriptionSet.find_or_initialize_by(
+          ceterms_ctid: binding.dig('ctid', 'value'),
+          path: path
+        )
+
+        description_set.uris = binding.dig('uris', 'value').split(' ')
+        description_set
+      end
+    end
+
+    def insert_description_sets(description_sets)
+      DescriptionSet.bulk_import(
+        description_sets,
+        on_duplicate_key_update: [:uris]
+      )
     end
 
     def maps
