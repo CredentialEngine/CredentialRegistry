fe4d61ac6e7340d454a67cab37a533bbf882817c
Alex Nizamov <nizamov.alex@gmail.com>
2022-06-02 13:39:56 +0500

    Prevent needless retries of IndexEnvelopeResourceJob

--- spec/jobs/index_envelope_resource_job_spec.rb
+++ spec/jobs/index_envelope_resource_job_spec.rb
@@ -4,9 +4,18 @@ RSpec.describe IndexEnvelopeResourceJob do
   describe '#perform' do
     let(:resource) { create(:envelope_resource) }
 
-    it 'calls IndexEnvelopeResource' do
-      expect(IndexEnvelopeResource).to receive(:call).with(resource)
-      IndexEnvelopeResourceJob.new.perform(resource.id)
+    context 'nonexistent resource ID' do
+      it 'calls IndexEnvelopeResource' do
+        expect(IndexEnvelopeResource).to receive(:call).with(resource)
+        IndexEnvelopeResourceJob.new.perform(resource.id)
+      end
+    end
+
+    context 'nonexistent resource ID' do
+      it 'does nothing' do
+        expect(IndexEnvelopeResource).not_to receive(:call)
+        IndexEnvelopeResourceJob.new.perform(0)
+      end
     end
   end
 end
