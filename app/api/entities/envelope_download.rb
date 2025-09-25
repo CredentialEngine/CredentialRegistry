c3718a8f14190c3057378898a172658569620fa3
Alex Nizamov <nizamov.alex@gmail.com>
2025-09-25 22:19:26 +0500

    (#860) Track time of envelope download job enqueuing

--- app/api/entities/envelope_download.rb
+++ app/api/entities/envelope_download.rb
@@ -8,6 +8,18 @@ module API
       expose :url,
              documentation: { type: 'string', desc: 'AWS S3 URL' },
              if: ->(object) { object.finished? }
+
+      expose :enqueued_at,
+             documentation: { type: 'string', desc: 'When the download was enqueued' },
+             if: ->(object) { object.pending? }
+
+      expose :finished_at,
+             documentation: { type: 'string', desc: 'When the download finished' },
+             if: ->(object) { object.finished? }
+
+      expose :started_at,
+             documentation: { type: 'string', desc: 'When the download started' },
+             if: ->(object) { object.in_progress? }
     end
   end
 end
