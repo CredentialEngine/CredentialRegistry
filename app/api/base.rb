166f2eb72ede6a8e31a1195bbd0a20b026ba2cc8
Ariel Rolfo <96243774+arielr-lt@users.noreply.github.com>
2025-10-31 15:34:52 -0300

    Use JsonRequestLogger for request logging
    Replaced custom logger middleware with JsonRequestLogger for JSON logging.

--- app/api/base.rb
+++ app/api/base.rb
@@ -1,18 +1,12 @@
 require 'v1/base'
 require 'v2/base'
+require_relative '../../lib/json_request_logger'
 
 module API
   # Main base class that defines all API versions
   class Base < Grape::API
-    insert_after Grape::Middleware::Formatter, Grape::Middleware::Logger, {
-      logger: MR.logger,
-      filter: Class.new do
-        def filter(opts)
-          opts['resource'] = '[FILTERED]' if opts['resource'] && MR.env == 'production'
-          opts
-        end
-      end.new
-    }
+    # Emit a single JSON log line per request
+    use JsonRequestLogger
 
     mount API::V1::Base
     mount API::V2::Base
