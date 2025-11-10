f5ae80fdc07320ad257cc15228470b23375cbed8
Alex Nizamov <nizamov.alex@gmail.com>
2025-11-10 17:06:33 +0500

    (#918) Increase ES max field limit dynamically

--- app/services/sync_envelope_graph_with_es.rb
+++ app/services/sync_envelope_graph_with_es.rb
@@ -32,17 +32,39 @@ class SyncEnvelopeGraphWithEs
     client.index(
       body: envelope.processed_resource.to_json,
       id: envelope_ceterms_ctid,
-      index: envelope_community.name
+      index: index_name
     )
 
     envelope.touch(:indexed_with_es_at)
+  rescue Elastic::Transport::Transport::Errors::BadRequest => e
+    raise e unless e.message.include?('Limit of total fields')
+
+    increase_total_fields_limit
+    retry
   end
 
   def delete
     return unless elasticsearch_address
 
-    client.delete(id: envelope_ceterms_ctid, index: envelope_community.name)
+    client.delete(id: envelope_ceterms_ctid, index: index_name)
   rescue Elastic::Transport::Transport::Errors::NotFound
     nil
+  end
+
+  def increase_total_fields_limit
+    settings = client.indices.get_settings(index: index_name)
+
+    current_limit = settings
+                    .dig(index_name, 'settings', 'index', 'mapping', 'total_fields', 'limit')
+                    .to_i
+
+    client.indices.put_settings(
+      body: { 'index.mapping.total_fields.limit' => current_limit * 2 },
+      index: index_name
+    )
+  end
+
+  def index_name
+    envelope_community.name
   end
 end
